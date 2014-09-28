c72af20c5cbe4d85dc87a6ba1d700848495fcad1
Nacha Escudero Leoz <miescuderol@gmail.com>
2014-09-28 15:47:59 -0300

    algunos avances menores en la interfaz grafica. Todavia el tree de la izquierda solo tiene imagen original y resultado, pero todavia no hay eventos asociados ni elementos para pasos intermedios.

--- src/simeav/Simeav.java
+++ src/simeav/Simeav.java
@@ -6,17 +6,42 @@
 
 package simeav;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 /**
  *
  * @author Nacha
  */
 public class Simeav {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    File imagenOriginal;
+    
+    Object setImagenOriginal(File selectedFile) {
+        imagenOriginal = selectedFile;
+        return imagenOriginal;
     }
+
+    void guardar(File selectedFile) {
+        PrintWriter writer;
+        try {
+            writer = new PrintWriter(selectedFile.getAbsolutePath(), "UTF-8");
+            writer.println("The first line");
+            writer.println("The second line");
+            writer.close();
+        } catch (FileNotFoundException | UnsupportedEncodingException ex) {
+            Logger.getLogger(Simeav.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    File getImagenOriginal() {
+        return imagenOriginal;
+    }
+
     
 }
